ec1d4dc9386392f2b6d1b3b67d69d846bc47f1d1
Simone Pasquini <simone.pasquini@gmail.com>
2017-04-24 16:59:11 +0200

    Refactored and introduce description and reason conditionals

--- src/Projection/CategoryExpenditureItems.php
+++ src/Projection/CategoryExpenditureItems.php
@@ -5,36 +5,66 @@ class CategoryExpenditureItems
 {
     const VARIABLE_COST_OTHER = 'Altro';
     const VARIABLE_COST_SONS = 'Spese/figli';
+    const VARIABLE_REVENUE_HUSBAND = 'HUSBAND';
+
+    const CATEGORY_POS_PAYMENT = 'PAGAMENTO TRAMITE POS';
+    const CATEGORY_ACCREDITATION_FEES = 'ACCREDITO EMOLUMENTI';
 
     private $categories;
+    private $description;
+    private $reason;
 
-    public function __construct()
+    public function __construct($description, $reason)
     {
         $this->categories = [
-            self::VARIABLE_COST_SONS => ['123456700088']
+            self::VARIABLE_COST_SONS => ['123456700088'],
         ];
+        $this->description = $description;
+        $this->reason = $reason;
     }
 
-    public function category($description)
+    public function category()
     {
-        $idPos = $this->idPos($description);
-
-        foreach($this->categories as $category => $listOfId) {
-            if (in_array($idPos, $listOfId)) {
-                return $category;
+        if (self::CATEGORY_POS_PAYMENT == $this->description) {
+            $idPos = $this->idPos($this->reason);
+            foreach($this->categories as $category => $listOfId) {
+                if (in_array($idPos, $listOfId)) {
+                    return $category;
+                }
             }
+            return self::VARIABLE_COST_OTHER;
         }
-        return self::VARIABLE_COST_OTHER;
+        if (self::CATEGORY_ACCREDITATION_FEES == $this->description) {
+            $idSalary = $this->idSalary();
+
+            return ('ORG:TREBI' == $idSalary) ?
+                self::VARIABLE_REVENUE_WIFE :
+                self::VARIABLE_REVENUE_HUSBAND
+            ;
+        }
     }
 
-    public function idPos($description)
+    private function idSalary()
+    {
+        $matches = [];
+        $pattern = '/ORD:([a-zA-Z0-9]*) /';
+        preg_match($pattern, $this->reason, $matches);
+        if (!isset($matches[1])) {
+            throw new CategoryExpenditureItemsException(
+                "ERROR: for {$this->reason} I can't identify SALARY Id"
+            );
+        }
+        return $matches[1];
+    }
+
+    public function idPos()
     {
         $matches = [];
         $pattern = '/C\/O ([0-9]*) /';
-        preg_match($pattern, $description, $matches);
+        preg_match($pattern, $this->reason, $matches);
         if (!isset($matches[1])) {
             throw new CategoryExpenditureItemsException(
-                "ERROR: for {$description} I can't identify POS Id"
+                "ERROR: for {$this->reason} I can't identify POS Id"
             );
         }
         return $matches[1];
--- tests/unit/Projection/CategoryExpenditureItemsTest.php
+++ tests/unit/Projection/CategoryExpenditureItemsTest.php
@@ -4,41 +4,58 @@ namespace Themis\Projection;
 
 class CategoryExpenditureItemsTest extends \PHPUnit_Framework_TestCase
 {
-    public function testShouldExtractPOSIdentificationCodeFromDescriptionField()
+    private $goodReason;
+    private $badReason;
+    private $posDescription;
+
+    public function setUp()
     {
-        $description = 'POS CARTA 01234567 DEL 27/03/17 ORE 13.19 C/O 123456700088 HOLDING DEI GIOCHI SPA - MILANO';
+        $this->posDescription = 'PAGAMENTO TRAMITE POS';
+        $this->goodReason = 'POS CARTA 01234567 DEL 27/03/17 ORE 13.19 C/O 123456700088 HOLDING DEI GIOCHI SPA - MILANO';
+        $this->badReason = 'POS CARTA 01234567 DEL 27/03/17 ORE 13.19 C/O123456700088 HOLDING DEI GIOCHI SPA - MILANO';
+    }
+
+    public function testShouldExtractPOSIdentificationCodeFromReasonField()
+    {
         $expected = '123456700088';
 
-        $expenditureItems = new CategoryExpenditureItems();
-        $this->assertEquals($expected, $expenditureItems->idPos($description));
+        $expenditureItems = new CategoryExpenditureItems($this->posDescription, $this->goodReason);
+        $this->assertEquals($expected, $expenditureItems->idPos());
     }
 
     /**
     * @expectedException Themis\Projection\CategoryExpenditureItemsException
     */
-    public function testShouldThrowExceptionWhenTheDescriptionNotContaintPOSId()
+    public function testShouldThrowExceptionWhenTheReasonNotContaintPOSId()
     {
-        $description = 'POS CARTA 01234567 DEL 27/03/17 ORE 13.19 C/O123456700088 HOLDING DEI GIOCHI SPA - MILANO';
-
-        $expenditureItems = new CategoryExpenditureItems();
-        $expenditureItems->idPos($description);
+        $expenditureItems = new CategoryExpenditureItems($this->posDescription, $this->badReason);
+        $expenditureItems->idPos();
     }
 
     public function testShouldKnowInWhichCategoryTheTransactionWillAppear()
     {
-        $description = 'POS CARTA 01234567 DEL 27/03/17 ORE 13.19 C/O 123456700088 HOLDING DEI GIOCHI SPA - MILANO';
         $expected = CategoryExpenditureItems::VARIABLE_COST_SONS;
 
-        $expenditureItems = new CategoryExpenditureItems();
-        $this->assertEquals($expected, $expenditureItems->category($description));
+        $expenditureItems = new CategoryExpenditureItems($this->posDescription, $this->goodReason);
+        $this->assertEquals($expected, $expenditureItems->category());
     }
 
     public function testShouldReturnOtherCategoryWhenIsUnknow()
     {
-        $description = 'POS CARTA 01234567 DEL 27/03/17 ORE 13.19 C/O 123456700086 HOLDING DEI GIOCHI SPA - MILANO';
+        $reason = 'POS CARTA 01234567 DEL 27/03/17 ORE 13.19 C/O 123456700086 HOLDING DEI GIOCHI SPA - MILANO';
         $expected = CategoryExpenditureItems::VARIABLE_COST_OTHER;
 
-        $expenditureItems = new CategoryExpenditureItems();
-        $this->assertEquals($expected, $expenditureItems->category($description));
+        $expenditureItems = new CategoryExpenditureItems($this->posDescription, $reason);
+        $this->assertEquals($expected, $expenditureItems->category());
+    }
+
+    public function testShouldRecognizeTheSalary()
+    {
+        $description = 'ACCREDITO EMOLUMENTI';
+        $reason = 'ORD:BIPONE SRL DT.ORD:000000 DESCR.OPERAZIONE SCT:ACCREDITOSTIPE';
+        $expected = CategoryExpenditureItems::VARIABLE_REVENUE_HUSBAND;
+
+        $expenditureItems = new CategoryExpenditureItems($description, $reason);
+        $this->assertEquals($expected, $expenditureItems->category());
     }
 }
