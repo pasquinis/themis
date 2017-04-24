965b8e683b59163aa9451d1f7f1db1a430445414
Simone Pasquini <simone.pasquini@gmail.com>
2017-04-24 12:11:06 +0200

    Added the possibility to extract the category

--- src/Projection/CategoryExpenditureItems.php
+++ src/Projection/CategoryExpenditureItems.php
@@ -3,6 +3,30 @@ namespace Themis\Projection;
 
 class CategoryExpenditureItems
 {
+    const VARIABLE_COST_OTHER = 'Altro';
+    const VARIABLE_COST_SONS = 'Spese/figli';
+
+    private $categories;
+
+    public function __construct()
+    {
+        $this->categories = [
+            self::VARIABLE_COST_SONS => ['123456700088']
+        ];
+    }
+
+    public function category($description)
+    {
+        $idPos = $this->idPos($description);
+
+        foreach($this->categories as $category => $listOfId) {
+            if (in_array($idPos, $listOfId)) {
+                return $category;
+            }
+        }
+        return self::VARIABLE_COST_OTHER;
+    }
+
     public function idPos($description)
     {
         $matches = [];
--- tests/unit/Projection/CategoryExpenditureItemsTest.php
+++ tests/unit/Projection/CategoryExpenditureItemsTest.php
@@ -23,4 +23,22 @@ class CategoryExpenditureItemsTest extends \PHPUnit_Framework_TestCase
         $expenditureItems = new CategoryExpenditureItems();
         $expenditureItems->idPos($description);
     }
+
+    public function testShouldKnowInWhichCategoryTheTransactionWillAppear()
+    {
+        $description = 'POS CARTA 01234567 DEL 27/03/17 ORE 13.19 C/O 123456700088 HOLDING DEI GIOCHI SPA - MILANO';
+        $expected = CategoryExpenditureItems::VARIABLE_COST_SONS;
+
+        $expenditureItems = new CategoryExpenditureItems();
+        $this->assertEquals($expected, $expenditureItems->category($description));
+    }
+
+    public function testShouldReturnOtherCategoryWhenIsUnknow()
+    {
+        $description = 'POS CARTA 01234567 DEL 27/03/17 ORE 13.19 C/O 123456700086 HOLDING DEI GIOCHI SPA - MILANO';
+        $expected = CategoryExpenditureItems::VARIABLE_COST_OTHER;
+
+        $expenditureItems = new CategoryExpenditureItems();
+        $this->assertEquals($expected, $expenditureItems->category($description));
+    }
 }
